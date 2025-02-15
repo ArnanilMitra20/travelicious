2d3f635a93ed7ff6e6de298df78d5e1fb0a43633
Arnanil <arnanilmitra06@gmail.com>
2025-02-15 22:30:16 +0530

    Authorization and Authentication

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ main()
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({...obj,owner:"67af04f5e04fb945126441b7"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
