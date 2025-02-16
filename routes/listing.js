5f5eff2c757d8794b053af9e38d56529275685db
Arnanil <arnanilmitra06@gmail.com>
2025-02-16 16:13:35 +0530

    Authorization and Authentication Completed

--- routes/listing.js
+++ routes/listing.js
@@ -1,20 +1,9 @@
 const express=require("express");
 const router=express.Router();
 const wrapAsync=require("../utils/wrapAsync.js");
-const {listingSchema}=require("../schema.js");
-const ExpressError=require("../utils/ExpressError.js");
 const Listing=require("../models/listing.js");
-const {isLoggedIn,isOwner}=require("../middleware.js")
+const {isLoggedIn,isOwner,validateListing}=require("../middleware.js")
 
-const validateListing = (req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-};
 
 //Index Route
 router.get("/",wrapAsync(async (req,res) =>{
@@ -30,7 +19,7 @@ router.get("/new",isLoggedIn,(req,res)=>{
 //Show Route
 router.get("/:id", wrapAsync(async (req, res) => {
     let {id}=req.params;
-    const listing=await Listing.findById(id).populate("reviews").populate("owner");
+    const listing=await Listing.findById(id).populate({path:"reviews",populate:{path:"author",},}).populate("owner");
     if(!listing){
         req.flash("error","The Listing you requested for, does not exist");
         res.redirect("/listings");
